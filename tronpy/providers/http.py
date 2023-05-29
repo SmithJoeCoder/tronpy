481552285394ffbd20f199109e2c4e3f8934cbe9
tn00378077 <joe.mercer118@gmail.com>
2023-05-29 17:14:39 +0800

    Adjust tronpy http file with AtomicCounter and remove codecov

--- tronpy/providers/http.py
+++ tronpy/providers/http.py
@@ -1,33 +1,63 @@
 import os
 import random
 import sys
+import threading
 import time
 from typing import Any, List, Union
 from urllib.parse import urljoin
 
 import requests
 
-from tronpy.version import VERSION
-
 DEFAULT_TIMEOUT = 10.0
 DEFAULT_API_KEYS = [
-    "f92221d5-7056-4366-b96f-65d3662ec2d9",
-    "1e0a625f-cfa5-43ee-ba41-a09db1aae55f",
-    "f399168e-2259-481c-90fc-6b3d984c5463",
-    "da63253b-aa9c-46e7-a4e8-22d259a8026d",
-    "88c10958-af7b-4d5a-8eef-6e84bf5fb809",
-    "169bb4b3-cbe8-449a-984e-80e9adacac55",
+    'f92221d5-7056-4366-b96f-65d3662ec2d9',
+    '1e0a625f-cfa5-43ee-ba41-a09db1aae55f',
+    'f399168e-2259-481c-90fc-6b3d984c5463',
+    'da63253b-aa9c-46e7-a4e8-22d259a8026d',
+    '88c10958-af7b-4d5a-8eef-6e84bf5fb809',
+    '169bb4b3-cbe8-449a-984e-80e9adacac55',
 ]
 
 
-class HTTPProvider:
+class AtomicCounter:
+    def __init__(self, initial=0, max_value=10):
+        """Initialize a new atomic counter to given initial value (default 0)."""
+        if initial > max_value:
+            raise ValueError("Initial value must be less than or equal to the max value")
+        if initial < 0:
+            raise ValueError("Initial value must be positive")
+        self.value = initial
+        self._lock = threading.Lock()
+        self.max_value = max_value
+
+    def incr(self, value=1):
+        """Atomically increment the counter by num (default 1) and return the
+        new value.
+        """
+        with self._lock:
+            if 0 <= self.value < self.max_value:
+                self.value += value
+            else:
+                self.reset()
+            return self.value
+
+    def reset(self):
+        self.value = 0
+
+    def __str__(self):
+        return str(self.value)
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class HTTPProvider(object):
     """An HTTP Provider for API request.
 
     :param endpoint_uri: HTTP API URL base. Default value is ``"https://api.trongrid.io/"``. Can also be configured via
         the ``TRONPY_HTTP_PROVIDER_URI`` environment variable.
     :param timeout: HTTP timeout in seconds.
     :param api_key: TronGRID API Key in str, or list of str.
-    :param jw_token: TronGRID JWT Credentials in str.
     """
 
     def __init__(
@@ -35,7 +65,6 @@ class HTTPProvider:
         endpoint_uri: Union[str, dict] = None,
         timeout: float = DEFAULT_TIMEOUT,
         api_key: Union[str, List[str]] = None,
-        jw_token: str = None,
     ):
         super().__init__()
 
@@ -46,9 +75,9 @@ class HTTPProvider:
         elif isinstance(endpoint_uri, (str,)):
             self.endpoint_uri = endpoint_uri
         else:
-            raise TypeError(f"unknown endpoint uri {endpoint_uri}")
+            raise TypeError("unknown endpoint uri {}".format(endpoint_uri))
 
-        if "trongrid" in self.endpoint_uri:
+        if 'trongrid' in self.endpoint_uri:
             self.use_api_key = True
             if isinstance(api_key, (str,)):
                 self._api_keys = [api_key]
@@ -57,23 +86,27 @@ class HTTPProvider:
             else:
                 self._api_keys = DEFAULT_API_KEYS.copy()
 
+            self._api_keys_length = len(self._api_keys)
+
+            # Choose the start index randomly.
+            self._api_keys_counter = AtomicCounter(
+                initial=random.randint(0, self._api_keys_length - 1),
+                max_value=self._api_keys_length - 1
+            )
+
             self._default_api_keys = self._api_keys.copy()
         else:
             self.use_api_key = False
-        self.jw_token = jw_token
 
         self.sess = requests.session()
-        self.sess.headers["User-Agent"] = f"Tronpy/{VERSION}"
+        self.sess.headers["User-Agent"] = "Tronpy/0.2"
 
         self.timeout = timeout
         """Request timeout in second."""
 
     def make_request(self, method: str, params: Any = None) -> dict:
         if self.use_api_key:
-            self.sess.headers["Tron-Pro-Api-Key"] = self.random_api_key
-
-        if self.jw_token is not None:
-            self.sess.headers["Authorization"] = f"Bearer {self.jw_token}"
+            self.sess.headers["Tron-Pro-Api-Key"] = self.serial_api_key  # Use serial api key to avoid collisions.
 
         if params is None:
             params = {}
@@ -81,8 +114,8 @@ class HTTPProvider:
         resp = self.sess.post(url, json=params, timeout=self.timeout)
 
         if self.use_api_key:
-            if resp.status_code == 403 and b"Exceed the user daily usage" in resp.content:
-                print("W:", resp.json().get("Error", "rate limit!"), file=sys.stderr)
+            if resp.status_code == 403 and b'Exceed the user daily usage' in resp.content:
+                print("W:", resp.json().get('Error', 'rate limit!'), file=sys.stderr)
                 self._handle_rate_limit()
                 return self.make_request(method, params)
 
@@ -92,6 +125,10 @@ class HTTPProvider:
     @property
     def random_api_key(self):
         return random.choice(self._api_keys)
+
+    @property
+    def serial_api_key(self):
+        return self._api_keys[self._api_keys_counter.incr()]
 
     def _handle_rate_limit(self):
         if len(self._api_keys) > 1:
